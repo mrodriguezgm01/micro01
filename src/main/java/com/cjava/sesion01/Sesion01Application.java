f5b1f8453677e6bc8c481d6d75e7e022c36d2956
MIC070418 <MIC070418@cjava.local>
2018-04-21 20:33:46 -0500

    sesion01 - add architype

---
+++ src/main/java/com/cjava/sesion01/Sesion01Application.java
@@ -0,0 +1,12 @@
+package com.cjava.sesion01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion01Application.class, args);
+	}
+}
---
+++ src/test/java/com/cjava/sesion01/Sesion01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cjava.sesion01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sesion01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
